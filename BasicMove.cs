43825c261e80737f5ac75476894a6d6e655e25ad
William Thai <54822136+wlli5@users.noreply.github.com>
2019-10-05 03:19:31 -0500

    Add files via upload
    Grunt AI and Player interaction with them.

---
+++ BasicMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMove : MonoBehaviour
+{
+    float time = 2;
+    public float speed;
+    bool hit = false;
+    float baseSpeed = 5;
+    public float hp = 10;
+ 
+    void OnCollisionEnter2D(Collision2D touched)
+    {
+        if (touched.gameObject.name == "Enemy")
+        {
+            speed -= 1;
+            hit = true;
+            time = 2;
+            hp -= 1;
+        }
+
+    }
+
+    void Update()
+    {
+        
+        if (hit)
+        {
+            time -= Time.deltaTime;
+        }
+        if (time <= 0)
+        {
+            speed = baseSpeed;
+        }
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
+
---
+++ Chase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    public float speed;
+    void OnCollisionEnter2D(Collision2D touched)
+    {
+        if (touched.gameObject.name == "bullet")
+        {
+            Destroy(this.gameObject);
+        }
+        if (touched.gameObject.name == "Main")
+        {
+            Vector3 pos = transform.position;
+            var MainPos = new Vector3(target.transform.position.x, target.transform.position.y, 0);
+            var moveEnemy = MainPos - pos;
+            transform.position += moveEnemy * speed * -Time.deltaTime*30;
+        }
+
+    }
+    // Update is called once per frame
+
+    void Update()
+    {
+       
+        Vector3 pos = transform.position;
+        var MainPos = new Vector3(target.transform.position.x, target.transform.position.y, 0);
+        var moveEnemy = MainPos - pos;
+        transform.position += moveEnemy * speed * Time.deltaTime;
+    }
+   
+}
